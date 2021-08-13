2d0286762e12462ca8eac50ef6fbc40a83e2ec95
Laár Eszter <laar.eszter@gmail.com>
2021-08-13 11:39:54 +0200

    ok

--- tests/login_test.py
+++ tests/login_test.py
@@ -13,41 +13,17 @@ driver.get('http://localhost:1667/')
 time.sleep(3)
 
 
-def find(xpath):
-    find = driver.find_element_by_xpath(xpath)
-    return find
-
-
-# Collection of xpath
-email_x = '//*[@id="app"]/div/div/div/div/form/fieldset[1]/input'
-pw_x = '//*[@id="app"]/div/div/div/div/form/fieldset[2]/input'
-username_x = '//*[@id="app"]/nav/div/ul/li[4]/a'
-sign_btn_x = '//*[@id="app"]/nav/div/ul/li[2]/a'
-sign_in_btn_x = '//*[@id="app"]/div/div/div/div/form/button'
-
-# Enter testdata
-email = 'testuser1@example.com'
-username = 'testuser1'
-pw = 'Abcd123$'
-
-
 # Sign in
-def sign_in():
-    sign_btn = find(sign_btn_x)
-    sign_btn.click()
-    find(email_x).send_keys(email)
-    find(pw_x).send_keys(pw)
-    sign_in_btn = find(sign_in_btn_x)
-    sign_in_btn.click()
-    time.sleep(2)
+def test_sign_in():
+    driver.find_element_by_xpath("//a[@href='#/login']").click()
+    driver.find_element_by_xpath('//*[@id="app"]/div/div/div/div/form/fieldset[1]/input').send_keys(
+        'testuser1@example.com')
+    driver.find_element_by_xpath('//*[@id="app"]/div/div/div/div/form/fieldset[2]/input').send_keys('Abcd123$')
+    driver.find_element_by_xpath('//*[@id="app"]/div/div/div/div/form/button').click()
+    time.sleep(3)
 
 
-sign_in()
-time.sleep(2)
-
-# Check login is managed
-
-your_feed_btn = driver.find_element_by_class_name('nav-link')
-assert your_feed_btn.is_enabled()
+test_sign_in()
+time.sleep(3)
 
 driver.close()
